89f45f2da1dbc059bd53233d82b329bf8b5db59f
willemwaasdorp <williewaas@gmail.com>
2015-04-21 10:05:43 +0000

    merge schoon

--- Opdracht1/ManyGreetings.py
+++ Opdracht1/ManyGreetings.py
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 naam = raw_input("Name? ")
 number = raw_input("How many greetings? ")
-print number + " x Hello, " + naam + "!"
-=======
-naam = raw_input("What is your name? ")
-print "Hello, " + naam + "!"
->>>>>>> 8d107616954036576dbc293ed23dedbc39f736be
+print number + " x Hello, " + naam 
+
+
