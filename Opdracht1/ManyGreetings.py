ef2b2f66f403af435fb12b53a814f82f29b68d24
willemwaasdorp <williewaas@gmail.com>
2015-04-21 10:00:57 +0000

    change

--- Opdracht1/ManyGreetings.py
+++ Opdracht1/ManyGreetings.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-naam = raw_input("Name? ")
+naam = raw_input("What is your name? ")
 number = raw_input("How many greetings? ")
 print number + " x Hello, " + naam + "!"
-=======
-naam = raw_input("What is your name? ")
-print "Hello, " + naam + "!"
->>>>>>> 8d107616954036576dbc293ed23dedbc39f736be
+
