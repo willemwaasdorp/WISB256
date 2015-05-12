110d42b93d09e73c4cea12d4c2d909e6088f6d68
willemwaasdorp <williewaas@gmail.com>
2015-05-12 10:45:41 +0000

    opdracht3v1

--- Opdracht3/estimate_pi.py
+++ Opdracht3/estimate_pi.py
@@ -1,39 +1,48 @@
 import sys
 import random
 import math
-L = sys.argv[2]
-n = sys.argv[1]
-if L == "" or n == "":
+try:
+    L = sys.argv[2]
+    n = sys.argv[1]
+except IndexError:
     print("Use: estimate_pi.py N L")
+    exit()
+try:
+    s = sys.argv[3]
+except IndexError:
+    s = ""
+
+L = int(L)
+n = int(n)
+if L > 1:
+    print("AssertionError: L should be smaller than 1")
+    exit()
 else:
-    L = int(L)
-    n = int(n)
-    if L > 1:
-        print("AssertionError: L should be smaller than 1")
-    else:
-        def drop_needle(L):
-            x = random.random()
-    
-            a = random.vonmisesvariate(0,0)
-            xn = x + L*math.cos(a)
-            if a < 0.5 * math.pi or a > 1.5 * math.pi:
-                if xn > math.ceil(x):
-                    return True
-                else:
-                    return False
-            elif a > 0.5 * math.pi or a < 1.5 * math.pi: 
-                if xn < math.floor(x):
-                    return True
-                else:
-                    return False
+    if s != "":
+        random.seed(s)
+    def drop_needle(L):
+        x = random.random()
+                
+        a = random.vonmisesvariate(0,0)
+        xn = x + L*math.cos(a)
+        if a < 0.5 * math.pi or a > 1.5 * math.pi:
+            if xn > math.ceil(x):
+                return True
+            else:
+                return False
+        elif a > 0.5 * math.pi or a < 1.5 * math.pi: 
+            if xn < math.floor(x):
+                return True
+            else:
+                return False
 
-        counter = 0
-        for i in range(n):
-            if drop_needle(L) == True:
-                counter += 1
+    counter = 0
+    for i in range(n):
+        if drop_needle(L) == True:
+            counter += 1
         
-        pi = (2 * L)/(counter/n)
+    pi = (2 * L)/(counter/n)
 
-        print(counter + " hits in " + n + " tries")
-        print("Pi = " + pi)
+    print(str(counter) + " hits in " + str(n) + " tries")
+    print("Pi = " + str(pi))
     
