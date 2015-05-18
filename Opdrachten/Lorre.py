aa9f1e374ec9e4c59164997b8fd1863dda9d73fe
willemwaasdorp <williewaas@gmail.com>
2015-05-18 09:04:33 +0000

    lorre

---
+++ Opdrachten/Lorre.py
@@ -0,0 +1,9 @@
+n = input()
+n = int(n)
+for i in range(n):
+    zin = input()
+    woorden = zin.split()
+    if len(woorden) > 4:
+        print("Crackers!")
+    else:
+        print(zin + " krAh!git ")
