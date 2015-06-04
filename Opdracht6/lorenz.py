cf71f169ac8d2e6fe7e8ffafd9e66acfb5be8ccb
willemwaasdorp <williewaas@gmail.com>
2015-06-04 14:25:29 +0000

    Opdracht6v2

--- Opdracht6/lorenz.py
+++ Opdracht6/lorenz.py
@@ -1,3 +1,9 @@
+import numpy as np
+from scipy.integrate import odeint 
 class Lorenz:
     def __init__(self, invoer, sigma=10.0, rho=28.0 , beta = 8/3.0):
+        self.x0 = invoer[0]
+        self.y0 = invoer[1]
+        self.z0 = invoer[2]
+    def solve(self, t,dt):
         pass
