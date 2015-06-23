f3e570fb181508cb97c529bde7f9c2eaafef6016
willemwaasdorp <williewaas@gmail.com>
2015-06-23 10:50:00 +0000

    eindv2

--- eindopdrachtv1/test.py
+++ eindopdrachtv1/test.py
@@ -2,30 +2,34 @@ import re
 import urllib.request
 
 textfile = open('depth_1.txt','wt')
-# print("Enter the URL you wish to crawl..")
-# print('Usage  - "http://phocks.org/stumble/creepy/" <-- With the double quotes')
-#myurl = input("@> ")
+
 website = 'http://nu.nl'
+domain = 'www.'+ website.replace("http://","")
 x = urllib.request.urlopen(website)
 for i in re.findall('''href=["'](.[^"']+)["']''', str(x.read()), re.I):
-        # print(i) 
+        
         if re.match('http', i) == None:
                 i = website + str(i)
-                #y = urllib.request.urlopen(website + str(i))
-        if re.search('nu.nl', i) == None:
-                next
-        else:
-                y = urllib.request.urlopen(i)
-        for ee in re.findall('''href=["'](.[^"']+)["']''', str(y.read()), re.I):
-                print(ee)       
-                textfile.write(ee+'\n')
+        
+        if (re.search(domain, i) != None) or (re.search(website, i) != None):
+                textfile.write(i+'\n')
+                 
+        else:        
+                
+                try:
+                        y = urllib.request.urlopen(str(i))
+                except urllib.error.URLError:
+                        continue
+                for ee in re.findall('''href=["'](.[^"']+)["']''', str(y.read()), re.I):
+                      
+                        
+                                
+                        if re.match('http', ee) == None:
+                                ee = i + str(ee)
+                                
+                             
+                        if (re.search(domain, ee) != None) or (re.search(website, ee) != None):
+                                textfile.write(ee+'\n')
+                             
         
         
-        # try:
-        #         
-        # except ValueError:
-        #         y = 'http://nu.nl' + str(i)
-        #         print(y)
-        
-#               
-# textfile.close()
