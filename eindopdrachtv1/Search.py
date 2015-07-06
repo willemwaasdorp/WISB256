1cb1873298a17114186a41c13c625becd2702f98
willemwaasdorp <williewaas@gmail.com>
2015-07-06 22:11:59 +0000

    finalv2

--- eindopdrachtv1/Search.py
+++ eindopdrachtv1/Search.py
@@ -16,22 +16,6 @@ lijst_y_coordinaten = []
 lijst_coordinaten = []
 scorelijst = []
 
-def tryint(s):
-    try:
-        return int(s)
-    except:
-        return s
-     
-def alphanum_key(s):
-    """ Turn a string into a list of string and number chunks.
-        "z23a" -> ["z", 23, "a"]
-    """
-    return [ tryint(c) for c in re.split('([0-9]+)', s) ]
-
-def sort_nicely(l):
-    """ Sort the given list in the way that humans expect.
-    """
-    return l.sort(key=alphanum_key)  
 
 def webscraping(page,nummer):
     try:
