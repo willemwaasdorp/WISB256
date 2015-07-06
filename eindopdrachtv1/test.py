c0ecb0d76bf375f861f9ef358d80941ef757af92
willemwaasdorp <williewaas@gmail.com>
2015-07-06 22:09:51 +0000

    final

--- eindopdrachtv1/test.py
+++ eindopdrachtv1/test.py
@@ -6,30 +6,8 @@ textfile = open('depth_1.txt','wt')
 website = 'http://www.stratic-advice.nl'
 domain = 'www.'+ website.replace("http://","")
 x = urllib.request.urlopen(website)
-for i in re.findall('''href=["'](.[^"']+)["']''', str(x.read()), re.I):
+for t in re.findall('<h3>.+</h3>',str(x.read()), re.I):
+        print(t)
         
-        if re.match('http', i) == None:
-                i = website + str(i)
-        
-        if (re.search(domain, i) != None) or (re.search(website, i) != None):
-                textfile.write(i+'\n')
-                 
-        else:        
-                
-                try:
-                        y = urllib.request.urlopen(str(i))
-                except urllib.error.URLError:
-                        continue
-                for ee in re.findall('''href=["'](.[^"']+)["']''', str(y.read()), re.I):
-                      
-                        
-                                
-                        if re.match('http', ee) == None:
-                                ee = i + str(ee)
-                                
-                             
-                        if (re.search(domain, ee) != None) or (re.search(website, ee) != None):
-                                textfile.write(ee+'\n')
-                             
-        
+
         
